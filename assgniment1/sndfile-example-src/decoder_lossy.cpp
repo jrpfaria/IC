516d15bd17b3819ad3edd938a5232cdc51ab4335
tiago@tiago-Vivobook-ASUSLaptop-X7600PC-N7600PC <tiago.r.santos@ua.pt>
2023-10-21 13:11:05 +0100

    start decoder (comment unnecessary)

--- assgniment1/sndfile-example-src/decoder_lossy.cpp
+++ assgniment1/sndfile-example-src/decoder_lossy.cpp
@@ -9,51 +9,19 @@ using namespace std;
 
 int main(int argc, char *argv[]) {
 
-	bool verbose { false };
-	size_t bs { 1024 };
-	double dctFrac { 0.2 };
+	// bool verbose { false };
+	// size_t bs { 1024 };
+	// double dctFrac { 0.2 };
 
-	if(argc < 3) {
-		cerr << "Usage: wav_dct [ -v (verbose) ]\n";
-		cerr << "               [ -bs blockSize (def 1024) ]\n";
-		cerr << "               [ -frac dctFraction (def 0.2) ]\n";
-		cerr << "               wavFileIn wavFileOut\n";
+	if (argc < 3) {
+		cerr << "Usage: " << argv[0] << " <input file> <output file>\n";
 		return 1;
 	}
 
-	for(int n = 1 ; n < argc ; n++)
-		if(string(argv[n]) == "-v") {
-			verbose = true;
-			break;
-		}
+	// get info from file (read bits)
+	string inputFile = argv[1];
+	BitStream bitstream(inputFile);
 
-	for(int n = 1 ; n < argc ; n++)
-		if(string(argv[n]) == "-bs") {
-			bs = atoi(argv[n+1]);
-			break;
-		}
-
-	for(int n = 1 ; n < argc ; n++)
-		if(string(argv[n]) == "-frac") {
-			dctFrac = atof(argv[n+1]);
-			break;
-		}
-
-	SndfileHandle sfhIn { argv[argc-2] };
-	if(sfhIn.error()) {
-		cerr << "Error: invalid input file\n";
-		return 1;
-    }
-
-	if((sfhIn.format() & SF_FORMAT_TYPEMASK) != SF_FORMAT_WAV) {
-		cerr << "Error: file is not in WAV format\n";
-		return 1;
-	}
-
-	if((sfhIn.format() & SF_FORMAT_SUBMASK) != SF_FORMAT_PCM_16) {
-		cerr << "Error: file is not in PCM_16 format\n";
-		return 1;
-	}
 
 	SndfileHandle sfhOut { argv[argc-1], SFM_WRITE, sfhIn.format(),
 	  sfhIn.channels(), sfhIn.samplerate() };
@@ -61,8 +29,8 @@ int main(int argc, char *argv[]) {
 		cerr << "Error: invalid output file\n";
 		return 1;
     }
-
-	if(verbose) {
+	
+	/*if(verbose) {
 		cout << "Input file has:\n";
 		cout << '\t' << sfhIn.frames() << " frames\n";
 		cout << '\t' << sfhIn.samplerate() << " samples per second\n";
@@ -71,13 +39,14 @@ int main(int argc, char *argv[]) {
 
 	size_t nChannels { static_cast<size_t>(sfhIn.channels()) };
 	size_t nFrames { static_cast<size_t>(sfhIn.frames()) };
-
+	*/
+	
 	// Read all samples: c1 c2 ... cn c1 c2 ... cn ...
 	// Note: A frame is a group c1 c2 ... cn
 	vector<short> samples(nChannels * nFrames);
 	sfhIn.readf(samples.data(), nFrames);
 
-	size_t nBlocks { static_cast<size_t>(ceil(static_cast<double>(nFrames) / bs)) };
+	// size_t nBlocks { static_cast<size_t>(ceil(static_cast<double>(nFrames) / bs)) };
 
 	// Do zero padding, if necessary
 	samples.resize(nBlocks * bs * nChannels);
@@ -87,20 +56,6 @@ int main(int argc, char *argv[]) {
 
 	// Vector for holding DCT computations
 	vector<double> x(bs);
-
-	// Direct DCT
-	fftw_plan plan_d = fftw_plan_r2r_1d(bs, x.data(), x.data(), FFTW_REDFT10, FFTW_ESTIMATE);
-	for(size_t n = 0 ; n < nBlocks ; n++)
-		for(size_t c = 0 ; c < nChannels ; c++) {
-			for(size_t k = 0 ; k < bs ; k++)
-				x[k] = samples[(n * bs + k) * nChannels + c];
-
-			fftw_execute(plan_d);
-			// Keep only "dctFrac" of the "low frequency" coefficients
-			for(size_t k = 0 ; k < bs * dctFrac ; k++)
-				x_dct[c][n * bs + k] = x[k] / (bs << 1);
-
-		}
 
 	// Inverse DCT
 	fftw_plan plan_i = fftw_plan_r2r_1d(bs, x.data(), x.data(), FFTW_REDFT01, FFTW_ESTIMATE);
