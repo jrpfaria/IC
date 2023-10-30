e99d7806046774ba996144d226d26de671d5768c
danielsilva12UA <104782036+danielsilva12UA@users.noreply.github.com>
2023-10-30 15:18:12 +0000

    some base code

--- assignment2/src/ppm_effects.cpp
+++ assignment2/src/ppm_effects.cpp
@@ -1,3 +1,15 @@
-int main() {
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+
+    // check number of args
+    if (argc < 4) {
+        cerr << "Usage: " << argv[0] << " <input file> <output file> <effect> <parameters>" << endl;
+        return 1;
+    }
+
     return 0;
 }
---
+++ assignment2/src/ppm_effects.h
@@ -0,0 +1,18 @@
+#ifndef WAVEFFECTS_H
+#define WAVEFFECTS_H
+
+#include <iostream>
+#include <vector>
+#include <math.h>
+#include <opencv.hpp>
+
+using namespace std;
+
+class PPMEffects {
+    private:
+
+    public:
+        
+}
+
+#endif
