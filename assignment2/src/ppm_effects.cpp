0642ad7b780a1a4ce4de97a4e4219a452ba153b1
rodrigofaria <jrfaria@ua.pt>
2023-11-06 15:57:18 +0000

    poggies

--- assignment2/src/ppm_effects.cpp
+++ assignment2/src/ppm_effects.cpp
@@ -7,12 +7,32 @@ using namespace std;
 using namespace cv;
 
 int main(int argc, char *argv[]) {
-
     // check number of args
-    if (argc < 4) {
-        cerr << "Usage: " << argv[0] << " <input file> <output file> <effect> <parameters>" << endl;
+    if (argc < 3) {
+        cerr << "Usage: " << argv[0] << " <input file> <output file> <channels>" << endl;
         return 1;
     }
+
+    // read input file
+    Mat image = imread(argv[1], IMREAD_COLOR);
+    if (image.empty()) {
+        cerr << "Could not open or find the image" << endl;
+        return 1;
+    }
+
+    // extract channels
+    // PPMEffects result = PPMEffects(argv[3][0]);
+    // image = result.extract(image);
+
+    image = PPMEffects::mirror(image, argv[4][0]);
+
+    // output image to ppm file
+    imwrite(argv[2], image);
+
+    // display result
+    namedWindow("Display window", WINDOW_AUTOSIZE);
+    imshow("Display window", image);
+    waitKey(0);
 
     return 0;
 }
--- assignment2/src/ppm_effects.h
+++ assignment2/src/ppm_effects.h
@@ -11,9 +11,73 @@ using namespace cv;
 
 class PPMEffects {
     private:
+        int color; // 0 = blue, 1 = green, 2 = red 
 
     public:
+        PPMEffects(char color) {
+            switch (color)
+            {
+                case 'b':
+                    this->color = 0;
+                    break;
+                case 'g':
+                    this->color = 1;
+                    break;
+                case 'r':
+                    this->color = 2;
+                    break;
+                default:
+                    this->color = 0; // Blue as default
+                    break;
+            }
+        }
 
+        Mat extract(Mat image) {
+            Mat result = Mat::zeros(image.rows, image.cols, CV_8UC1);
+            for (int i = 0; i < image.rows; i++) {
+                for (int j = 0; j < image.cols; j++) {
+                    result.at<uchar>(i,j) = image.at<Vec3b>(i,j)[this->color];
+                }
+            }
+            return result;
+        }
+
+        Mat negative(Mat image) {
+            Mat result = Mat::zeros(image.rows, image.cols, CV_8UC3);
+            for (int i = 0; i < image.rows; i++)
+                for (int j = 0; j < image.cols; j++){
+                    result.at<Vec3b>(i,j)[0] = 0xFF - image.at<Vec3b>(i,j)[0];
+                    result.at<Vec3b>(i,j)[1] = 0xFF - image.at<Vec3b>(i,j)[1];
+                    result.at<Vec3b>(i,j)[2] = 0xFF - image.at<Vec3b>(i,j)[2];
+                }
+            return result;
+        }
+
+        static Mat mirror(Mat image, unsigned char orientation) {
+            return orientation == 'v' ? mirrorVertically(image) : mirrorHorizontally(image);
+        }
+
+        static Mat mirrorHorizontally(Mat image) {
+            Mat result = Mat::zeros(image.rows, image.cols, CV_8UC3);
+            for (int i = 0; i < image.rows; i++)
+                for (int j = 0; j < image.cols; j++){
+                    result.at<Vec3b>(i,j)[0] = image.at<Vec3b>(i,image.cols - j)[0];
+                    result.at<Vec3b>(i,j)[1] = image.at<Vec3b>(i,image.cols - j)[1];
+                    result.at<Vec3b>(i,j)[2] = image.at<Vec3b>(i,image.cols - j)[2];
+                }
+            return result;
+        }
+
+        static Mat mirrorVertically(Mat image) {
+            Mat result = Mat::zeros(image.rows, image.cols, CV_8UC3);
+            for (int i = 0; i < image.rows; i++)
+                for (int j = 0; j < image.cols; j++){
+                    result.at<Vec3b>(i,j)[0] = image.at<Vec3b>(image.rows - i,j)[0];
+                    result.at<Vec3b>(i,j)[1] = image.at<Vec3b>(image.rows - i,j)[1];
+                    result.at<Vec3b>(i,j)[2] = image.at<Vec3b>(image.rows - i,j)[2];
+                }
+            return result;
+        }
 };
 
 #endif
