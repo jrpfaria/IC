88507ca09933b95dd5f9d62a42cbf5506500155a
rodrigofaria <jrfaria@ua.pt>
2023-11-15 23:17:03 +0000

    golomb encoder magnitude and signal

--- assignment2/src/golomb.h
+++ assignment2/src/golomb.h
@@ -11,76 +11,79 @@ using namespace cv;
 
 class Golomb {
     private:
-
         int m;
-        bool sign_and_magnitude = true;
+        bool method = 1; // sign and magnitude; false = two's complement
 
     public:
-        Golomb(int m){
+        Golomb(int m, bool method){
             this->m = m;
+            this->method = method;
         }
 
-        vector<bool> encode(int x) {
+        vector<bool> encode(int i) {
             vector<bool> bits;
+            int q, r, b;
 
             // Encode the sign bit first.
-            bits.push_back(x < 0);
+            bits.push_back(i < 0);
 
-            // Encode the absolute value of x.
-            int abs_x = abs(x);
-            int quotient = abs_x / m;
-            int remainder = abs_x % m;
+            // Encode the absolute value of i.
+            i = abs(i);
+            q = floor(i / m);
+            r = i % m;
 
-            // Encode the quotient as a unary code.
-            for (int i = 0; i < quotient; i++) {
-                bits.push_back(true);
-            }
-            bits.push_back(false);
+            // Encode the quotient: unary code.
+            for (int j = 0; j < q; j++)
+                bits.push_back(1);
+            bits.push_back(0);
 
-            // Encode the remainder in binary.
-            for (int i = m - 1; i >= 0; i--) {
-                bits.push_back((remainder >> i) & 1);
-            }
+            // Encode the remainder: binary code
+            // Check the need to use truncated binary.
+            bool truncated = log2(m) != ceil(log2(m));
+            b = ceil(log2(m));
+
+            if (truncated)
+                if (r >= (1 << b) - m){
+                    r = r + (1 << b) - m;
+                    for (int j = b - 1; j >= 0; j--)
+                        bits.push_back((r >> j) & 1);
+                }
+                else
+                    for (int j = (b - 1) - 1; j >= 0; j--)
+                        bits.push_back((r >> j) & 1);
+            else
+                for (int j = b - 1; j >= 0; j--)
+                    bits.push_back((r >> j) & 1);
 
             return bits;
         }
 
         // Decodes a sequence of bits into an integer.
         int decode(vector<bool> bits) {
-            int x = 0;
+            int i = 0;
 
             // Decode the sign bit first.
-            x = bits[0] ? -1 : 1;
+            i = bits[0] ? -1 : 1;
 
             // Decode the absolute value of x.
-            int quotient = 0;
-            int remainder = 0;
+            int q = 0;
+            int r = 0;
 
             // Decode the quotient from the unary code.
-            int i = 1;
-            while (bits[i]) {
-                quotient++;
-                i++;
+            int j = 1;
+            while (bits[j]) {
+                q++;
+                j++;
             }
 
             // Decode the remainder from the binary code.
             for (int j = 0; j < m; j++)
-                remainder |= bits[i + j] << (m - j - 1);
+                r |= bits[i + j] << (m - j - 1);
             
 
-            x *= quotient * m + remainder;
+            i *= q * m + r;
 
-            return x;
-        }
-
-        // Sets the Golomb parameter, m.
-        void set_m(int m) {
-            m = m;
-        }
-
-        // Sets the sign and magnitude encoding mode.
-        void set_sign_and_magnitude(bool sign_and_magnitude) {
-            this->sign_and_magnitude = sign_and_magnitude;
+            return i;
         }
 };
 
