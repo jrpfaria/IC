4ce5f789dfd88c74fd691edef639b31885005acb
rodrigofaria <jrfaria@ua.pt>
2023-11-17 09:14:53 +0000

    changes to golomb.cpp and better script for testing golomb

--- assignment2/src/golomb.cpp
+++ assignment2/src/golomb.cpp
@@ -9,12 +9,25 @@ using namespace cv;
 int main(int argc, char *argv[]) {
   
   if (argc < 2) {
-        cerr << "Usage: " << argv[0] << " <int>"<< endl;
-        return 1;
-    }
-  
+    cerr << "Usage: " << argv[0] << " <int> <m> <method> "<< endl;
+    return 1;
+  }
+
+  int m = stoi(argv[2]);
+  int method = stoi(argv[3]);
+
+  if (m < 1) {
+    cerr << "M must be greater than 0" << endl;
+    return 1;
+  }
+
+  if (method != 0 && method != 1) {
+    cerr << "Method must be boolean value 0 (value intervaling) or 1 (signal and magnitude)" << endl;
+    return 1;
+  }
+
   // Create a GolombCoder with the parameter m = 4.
-  Golomb coder(5, 0);
+  Golomb coder(m, method);
 
   // Encode the integer 15.
   vector<bool> bits = coder.encode(stoi(argv[1]));
